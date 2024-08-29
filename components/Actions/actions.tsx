6b7d6a142a980f83e1e9184ae006480f61ccef21
andersonFaro9 <faro.anderson.git@gmail.com>
2024-08-29 15:03:13 -0300

    alinhamento textos de baixo

--- components/Actions/actions.tsx
+++ components/Actions/actions.tsx
@@ -30,25 +30,24 @@ const Actions = () => {
       {users.map((users) => {
         return (
           <TouchableOpacity key={users.id} onPress={() => {}}>
-            <View key={users.id} style={styles.user}>
-              <View style={styles.info}>
-                <View>
-                  <Text style={styles.title}>Magazine Luiza</Text>
-                  <Text style={styles.subtitle}>MGLU3</Text>
-                </View>
-                
+            <View style={styles.success}>
+              <View style = {styles.line}>
+                <Text style={styles.name}>Magazine Luiza</Text>
+                <Text style={styles.ticker}>MGLU3</Text>
+                <Text style={styles.minimum}>Valor minimo</Text>
+                <Text style={styles.minimum}>Rentabilidade</Text>
+              </View>
 
+              <View>
                 <Image
                   style={styles.image}
                   resizeMode='cover'
                   source={users.avatar}
                 />
+                
+                <Text style={styles.value}>R$24,17</Text>
+                <Text>-27%</Text>
               </View>
-              {/* <View style={styles.title2}>
-                <Text numberOfLines={1}>________________</Text>
-              </View> */}
-
-              
             </View>
           </TouchableOpacity>
         )
@@ -58,29 +57,15 @@ const Actions = () => {
 }
 
 const styles = StyleSheet.create({
-  info: {
+  success: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
+    gap: 142,
+    
     fontSize: 16,
-    gap: 46,
     width: 350,
     paddingTop: 12,
     paddingLeft: 12,
-  },
-
-  title2: {
-    flexDirection: 'column',
-    borderBottomColor: 'black',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-
-  user: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: 371,
-    gap: 92,
-
-    alignItems: 'center',
     borderRadius: 14,
     borderColor: '#1111',
     backgroundColor: 'white',
@@ -89,26 +74,49 @@ const styles = StyleSheet.create({
     height: 160,
   },
 
-  title: {
+  line : {
+    borderCurve:'circular' ,
+    
+  },
+  value: {
+    flexDirection: 'column',
+    paddingTop: 42,
+    
+  },
+  name: {
     color: '#000000',
     fontWeight: 'bold',
     fontSize: 17,
   },
-  subtitle: {
+
+  image: {
+    width: 40,
+    height: 40,
+  },
+  ticker: {
     color: '#000000',
     fontWeight: '700',
-    paddingTop: 2,
+    paddingTop: 0,
+    marginBottom: 11,
     paddingBottom: 32,
+    
+    
   },
 
+  minimum: {
+    gap: 222,
+    
+  },
+
+  minimumValue: {
+    color: '#000000',
+    fontWeight: 'normal',
+    fontSize: 17,
+  },
   container: {
     alignItems: 'center',
 
     justifyContent: 'center',
-  },
-  image: {
-    width: 40,
-    height: 40,
   },
 })
 
